c6bf92839b9af9b33321ce14a9934fa4e116e293
henriqfelix <henriquefelix1821@gmail.com>
2022-06-21 11:28:08 -0300

    adicionar seção de skills

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,22 @@ const scrollHeader = () => {
     : header.classList.remove("scroll-header");
 };
 window.addEventListener("scroll", scrollHeader);
+
+/*==== ACCORDION SKILLS ====*/
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__closed";
+  }
+  if (itemClass === "skills__content skills__closed") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
