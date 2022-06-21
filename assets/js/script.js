e6d787bc214d179d1dcc931e42565f4c68ead879
henriqfelix <henriquefelix1821@gmail.com>
2022-06-21 14:30:12 -0300

    adicionar section projetos

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,39 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
   el.addEventListener("click", toggleSkills);
 });
+
+/*==== PROJECT SWIPPER ====*/
+let swiper = new Swiper(".project-swiper", {
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // init: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    400: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    680: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    950: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1100: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
